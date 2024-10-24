7c4030fb551bea63d955022d958a13791d721c13
DAVID BERTO FARIAS <48934991+DavidBerto@users.noreply.github.com>
2024-10-24 12:59:15 -0300

    Update 1_home.py
    no date

--- app/pages/1_home.py
+++ app/pages/1_home.py
@@ -4,8 +4,8 @@ st.title("Bem vindo ao Descritor de Alimentos")
 st.write("Este aplicativo te ajuda a acompanhar sua evolução")
 st.write("Acesse a função desejada pelo menu à esquerda")
 
-d = st.date_input(label_visibility=True)
-d
+#d = st.date_input(label_visibility=True)
+#d
 
 # noticias do andamento dos pacientes
 #destacar os pacientes criticos
