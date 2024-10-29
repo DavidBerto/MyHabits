be922a07fe382b5ea261c10f10b7e506c46635d9
david <david.farias064@gmail.com>
2024-10-28 22:02:57 -0300

    links

--- app/pages/paciente1.py
+++ app/pages/paciente1.py
@@ -95,8 +95,8 @@ def filter_dataframe(df, search_term, filters):
 
 col1, col2, col3 = st.columns([0.2,0.2, 0.4])
 
-#image = Image.open("/mount/src/myhabits/app/images/foto_david.jpg")
-image = Image.open("C:/Users/david/OneDrive/Projetos/MyHabits/app/images/foto_david.jpg")
+image = Image.open("/mount/src/myhabits/app/images/foto_david.jpg")
+#image = Image.open("C:/Users/david/OneDrive/Projetos/MyHabits/app/images/foto_david.jpg")
 #foto
 col1.image(image,width = 200)
 feedback = col1.feedback('stars')
